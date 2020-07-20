9c7dc431460f355091b7bd27f2262dbf8bbdbf0c
Jorge Salazar López <jorge005@hotmail.com>
2020-07-20 18:43:54 -0500

    Finalizado video 09

--- Soccer.Web/Data/DataContext.cs
+++ Soccer.Web/Data/DataContext.cs
@@ -12,6 +12,25 @@ namespace Soccer.Web.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
         }
+
+        public DbSet<GroupDetailEntity> GroupDetails { get; set; }
+
+        public DbSet<GroupEntity> Groups { get; set; }
+
+        public DbSet<MatchEntity> Matches { get; set; }
+
         public DbSet<TeamEntity> Teams { get; set; }
+
+        public DbSet<TournamentEntity> Tournaments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<TeamEntity>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
+
     }
 }
