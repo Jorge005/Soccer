3b1938cfbe3a9794d7ba63c9a808c058be81c3f5
Jorge Salazar López <jorge005@hotmail.com>
2020-07-25 12:24:03 -0500

    Video 12

---
+++ Soccer.Web/Controllers/TournamentsController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Soccer.Web.Data;
+using Soccer.Web.Helpers;
+using Soccer.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Soccer.Web.Controllers
+{
+    public class TournamentsController : Controller
+    {
+        private readonly IConverterHelper _converterHelper;
+        private readonly DataContext _context;
+        private readonly IImageHelper _imageHelper;
+
+        public TournamentsController(
+            IConverterHelper converterHelper,
+            DataContext context,
+            IImageHelper imageHelper
+            )
+        {
+            _converterHelper = converterHelper;
+            _context = context;
+            _imageHelper = imageHelper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context
+                .Tournaments
+                .Include(t => t.Groups)
+                .OrderBy(t => t.StartDate)
+                .ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(TournamentViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string path = string.Empty;
+
+                if (model.LogoFile != null)
+                {
+                    path = await _imageHelper.UploadImageAsync(model.LogoFile, "Tournaments");
+                }
+
+                var tournament = _converterHelper.ToTournamentEntity(model, path, true);
+                _context.Add(tournament);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(model);
+        }
+
+    }
+}
